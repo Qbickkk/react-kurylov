f4d34b20266444908f5fee7dfd959105b7f88965
shulika <illia.shulika@gmail.com>
2024-04-07 17:31:19 -0700

    preview 1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
 
+import {MainComponent} from "./components/MainComponent";
+
 const App = () => {
+
+
+    const world = (value) => `world ${value}`;
+
+    const mainTitle = 'Chinazesss';
+
     return (
-        <div>
-            App
-        </div>
+      <div>
+          <h1>Hello {world('Jamaika')}</h1>
+          <MainComponent title={mainTitle}/>
+      </div>
     );
 };
 
---
+++ src/components/MainComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MainComponent = ({title}) => {
+    return (
+        <div>
+            {title}
+        </div>
+    );
+};
+
+export {MainComponent};
